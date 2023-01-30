8e01de7bf895525308f592f4635aa4074a9152ca
alinalt28 <alina.anishenko@yandex.by>
2023-01-30 04:49:43 +0300

    less 7

--- les1.py
+++ les1.py
@@ -1,2 +1,37 @@
-hghghghg
+chis1 = 1
+chis2 = 100
+print(chis1**chis2)
+
+chis3 = 2
+chis4 = 16
+print(chis3**chis4)
+
+chis5 = 1000
+print(chis3-chis5)
+
+print(chis3+chis3*chis3)
+
+chis6 = 60
+chis7 = 3
+print(chis6/chis7)
+
+import math
+print(math.sqrt(144))
+
+chis8 = 14
+chis9 = 5
+str1 = "x="
+per1 = (chis3+chis9*chis8)
+print(str1, per1)
+
+chis10 = 66
+chis11 = 11
+per2 = (chis10-chis11)
+print(str1, per2)
+
+chis12 = 16
+chis13 = 8
+print(str1, math.sqrt(chis12-chis13))
+
+
 
